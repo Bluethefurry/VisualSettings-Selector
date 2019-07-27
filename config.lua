9232b292c2154775a6c19e2465fd0bc2b3838862
Blue <ericmager@gmx.de>
2019-07-27 11:09:11 +0200

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,9 +1,9 @@
 files = {
 	{file = "default.dat", label = "Default", default = true}, 
 	-- make sure to also add these in the __resource.lua
-	-- file = filename incl. folder path, if any, default = "reset to default" button will reset it to that file
-	-- always update the default.dat file in case of game updates
-	-- set = gets set automatically when joining
+	-- file = filename inside /data, 
+	-- default = "reset to default" button will reset it to that file, don't change that
+	-- set = gets set automatically when joining ( not finished )
 
 	{file = "visualv.dat", label = "VisualV", set = false}, -- visuallv file.
 	{file = "raturalrealism.dat", label = "Natural Realism", set = false}, -- blü's visualsettings
